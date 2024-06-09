8a21414fb5bb5dd49a225a0e007d0f25304bd6d3
nyellll <159218653+nyellll@users.noreply.github.com>
2024-06-10 01:13:33 +0800

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,127 @@
+﻿Imports System.Drawing.Drawing2D
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
+Imports System.Data.OleDb
+Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Const CONNECTION_STRING As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Danial\source\repos\revis\Database21.mdb"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Create a new connection
+        Using conn As New OleDbConnection(CONNECTION_STRING)
+            Try
+                ' Open the connection
+                conn.Open()
+
+                ' Create a new data adapter based on the specified query.
+                Dim dataAdapter As New OleDbDataAdapter("SELECT * FROM Table1", conn)
+
+                ' Create a command builder to generate SQL update, insert, and
+                ' delete commands based on select command. These are used to
+                ' update the database.
+                Dim commandBuilder As New OleDbCommandBuilder(dataAdapter)
+
+                ' Populate a new data table and bind it to the BindingSource.
+                Dim table As New DataTable()
+                table.Locale = System.Globalization.CultureInfo.InvariantCulture
+                dataAdapter.Fill(table)
+
+                ' Bind the table to the DataGridView
+                DataGridView1.DataSource = table
+
+            Catch ex As Exception
+                MessageBox.Show("An error occurred while loading the data: " & ex.Message)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub ReferenceToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ReferenceToolStripMenuItem.Click
+        MessageBox.Show("This Created by HambaliFaris")
+    End Sub
+
+    Private Sub btnSD_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If String.IsNullOrEmpty(txtName.Text) OrElse String.IsNullOrEmpty(cboGender.Text) OrElse String.IsNullOrEmpty(txtPhone.Text) OrElse String.IsNullOrEmpty(cboCourse.Text) OrElse String.IsNullOrEmpty(txtSem.Text) OrElse String.IsNullOrEmpty(txtFee.Text) Then
+                MessageBox.Show("Please fill all fields")
+                Return
+            End If
+
+            Dim name As String
+            name = txtName.Text
+
+            Dim gender As String
+            gender = cboGender.Text
+
+            Dim phone As String
+            phone = UCase(txtPhone.Text)
+
+            Dim course As String
+            course = cboCourse.Text
+
+            Dim semester As String
+            semester = UCase(txtSem.Text)
+
+            Dim fee As String
+            fee = UCase(txtFee.Text)
+
+            txtShow.Text = "Name : " + name.ToString + vbNewLine + "Gender : " + gender.ToString +
+            vbNewLine + "Phone Number : " + phone.ToString + vbNewLine + "Course : " + course.ToString +
+            vbNewLine + "Semester : " + semester.ToString + vbNewLine + "Fee : RM" + fee.ToString
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim Name As String = txtName.Text
+        Dim Gender As String = cboGender.Text
+        Dim Phone As String = txtPhone.Text
+        Dim Course As String = cboCourse.Text
+        Dim Semester As String = txtSem.Text
+        Dim Fee As String = txtFee.Text
+
+        Dim sql As String = "INSERT INTO Table1 ([Name],[Gender],[Phone],[Course],[Semester],[Fee]) VALUES (@Name,@Gender,@Phone,@Course,@Semester,@Fee)"
+        Try
+            conn.Open()
+
+            cmd = conn.CreateCommand
+            cmd.Connection = conn
+
+            cmd.CommandText = sql
+
+            cmd.Parameters.AddWithValue("@Name", Name)
+            cmd.Parameters.AddWithValue("@Gender", Gender)
+            cmd.Parameters.AddWithValue("@Phone", Phone)
+            cmd.Parameters.AddWithValue("@Course", Course)
+            cmd.Parameters.AddWithValue("@Semester", Semester)
+            cmd.Parameters.AddWithValue("@Fee", Fee)
+
+            cmd.ExecuteNonQuery()
+
+            MessageBox.Show("Data inserted successfully!")
+        Catch ex As Exception
+            MessageBox.Show("An error occurred: " + ex.Message)
+        Finally
+            ' Close the connection
+            If conn IsNot Nothing AndAlso conn.State = ConnectionState.Open Then
+                conn.Close()
+            End If
+        End Try
+    End Sub
+
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+End Class
+
+
+
+
+
+95beb-707c9
